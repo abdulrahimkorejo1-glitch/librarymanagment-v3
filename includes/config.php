963e2fd2a1464ae66fcb7fc8ba798fbf4b695d0d
Abdul rahim Korejo <abdulrahimkorejo1@gmail.com>
2026-08-24 11:33:18 -0700

    Change database connection settings
    Updated database connection parameters to use remote server.

--- includes/config.php
+++ includes/config.php
@@ -1,14 +1,10 @@
-<?php
-
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "library_db";
+$host = "sql106.infinityfree.com";
+$user = "if0_42740575";
+$pass = "APNA_INFINITYFREE_MYSQL_PASSWORD";
+$dbname = "if0_42740575_librarydb";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
     die("Connection Failed: " . $conn->connect_error);
 }
-
-?>
